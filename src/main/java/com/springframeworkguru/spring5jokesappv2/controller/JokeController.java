a24ad151525f4eb7e9c3ba6dec589218cafe2da5
adriana.guerrasio <adriana.guerrasio@evaluate.com>
2022-12-08 17:16:34 -0300

    Create Controller JokeController. Closes #4.

---
+++ src/main/java/com/springframeworkguru/spring5jokesappv2/controller/JokeController.java
@@ -0,0 +1,24 @@
+package com.springframeworkguru.spring5jokesappv2.controller;
+
+import com.springframeworkguru.spring5jokesappv2.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired //to do dependency injections
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "index";
+    }
+}
